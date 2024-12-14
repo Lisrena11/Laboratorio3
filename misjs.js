8a32deb6f9e92dc148da818283b599c6a49e84fa
Renata <lisrenata11@gmail.com>
2024-12-14 13:35:34 -0500

    misjs.js

--- misjs.js
+++ misjs.js
@@ -1,7 +1,7 @@
 
 
 function segura() {
-    pwd = prompt( "Contraseñaa" ) ;
+    pwd = prompt( "Contraseña" ) ;
 
     vcv = 0 ;       // CONTADORA DE VOCALES
     vcc = 0 ;       // CONTADORA DE CONSONANTES
